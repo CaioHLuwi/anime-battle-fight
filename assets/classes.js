f372b5885cf7cfb94b45d8d33694b3eb78aca537
Caio Henrique <caiohenriquelw2004@gmail.com>
2024-11-21 16:03:48 -0300

    add visual log in website screen when character attacks and defends + audio in win

--- assets/classes.js
+++ assets/classes.js
@@ -38,12 +38,13 @@ class Tomioka extends Character {
 }
 
 class Phase {
-    constructor(ally, enemy, allyStatus, enemyStatus, characterElements) { // Traz todos os elementos do HTML para serem selecionados pelos métodos.
+    constructor(ally, enemy, allyStatus, enemyStatus, characterElements, log) { // Traz todos os elementos do HTML para serem selecionados pelos métodos.
         this.ally = ally;
         this.enemy = enemy;
         this.allyElement = allyStatus;
         this.enemyElement = enemyStatus;
         this.characterElements = characterElements;
+        this.log = log;
     }
     
 
@@ -57,6 +58,19 @@ class Phase {
     }
 
     update(){
+
+        let audioWins = new Audio('/assets/sounds/victory_sound.mp3');
+        // Verifica se algum dos personagens venceu a luta.
+        if(this.ally.life <= 0){
+            this.log.addMessage(`----- ${this.enemy.name} venceu! -----`);
+            this.log.listElement.children[0].classList.add('wins'); // Adiciona a classe "wins" para deixar o texto verde.
+            audioWins.play();
+        } else if (this.enemy.life <= 0){
+            this.log.addMessage(`----- ${this.ally.name} venceu! -----`)
+            this.log.listElement.children[0].classList.add('wins'); // Adiciona a classe "wins" para deixar o texto verde.
+            audioWins.play();
+        }
+        
         // Trocando nome de cada um dos personagens
         this.allyElement.querySelector('#nameCharacter1').innerText = this.ally.name;
 
@@ -68,11 +82,11 @@ class Phase {
         this.enemyElement.querySelector('.bar').style.width = `${(this.enemy.life / this.enemy.maxLife) * 100}%`;
 
         // Definir os atributos no css
-        this.allyElement.querySelector('#life').innerText = ally.life.toFixed(2);
+        this.allyElement.querySelector('#life').innerText = ally.life.toFixed(0);
         this.allyElement.querySelector('#power').innerText = ally.power;
         this.allyElement.querySelector('#defense').innerText = ally.defense;
 
-        this.enemyElement.querySelector('#life').innerText = enemy.life.toFixed(2);
+        this.enemyElement.querySelector('#life').innerText = enemy.life.toFixed(0);
         this.enemyElement.querySelector('#power').innerText = enemy.power;
         this.enemyElement.querySelector('#defense').innerText = enemy.defense;
     }
@@ -90,15 +104,39 @@ class Phase {
         let actualPower = attacking.power * powerFactor; // Número aleatório multiplicando o poder atual do atacante para dar randomização ao jogo
         let actualDefense = attacked.defense * defenseFactor; // Número aleatório multiplicando a defesa atual do defensor para dar randomização ao jogo
 
-        if(actualPower > actualDefense) {
-            attacking.life -= actualPower
-            console.log(`${attacking.name} conseguiu atacar ${attacked.name} e causou ${actualPower}`);
+        let powerConverted = Math.round(Number(actualPower));
+        let defenseConverted = Math.round(Number(actualDefense));
+        
+        if(powerConverted > defenseConverted) {
+            attacked.life -= actualPower;
+            this.log.addMessage(`${attacking.name} causou ${powerConverted} de dano`);
         } else {
-            console.log(`${attacked.name} conseguiu defender.`)
+            this.log.addMessage(`${attacking.name} lançou um ataque e ${attacked.name} conseguiu <strong> defender. <strong>`);
         }
 
-        //TODO: Implementar um log para receber todos esses registros e melhores imagens / sons no projeto.
+        //TODO: Implementar um log para receber todos esses registros e melhores imagens / sons no projeto
 
         this.update()
+    }
+}
+
+class Log {
+    list = [];
+
+    constructor(listElement){
+        this.listElement = listElement;
+    }
+
+    addMessage(message){
+        this.list.unshift(message);
+        this.newItem();
+    }
+
+    newItem(){
+        this.listElement.innerHTML = '';
+
+        for(let i in this.list){
+            this.listElement.innerHTML += `<li class='log-item'> ${this.list[i]}<li>`
+        }
     }
 }
--- assets/script.js
+++ assets/script.js
@@ -3,13 +3,14 @@ let enemy = new Tomioka();
 let statusAlly = document.querySelector('#ally-status');
 let statusEnemy = document.querySelector('#enemy-status');
 let characterElements = document.querySelector('.scenario-char');
-
+let log = new Log(document.querySelector('#log'));
 let phase = new Phase(
     ally,
     enemy,
     statusAlly,
     statusEnemy,
-    characterElements
+    characterElements,
+    log
 )
 
 phase.start();
